2464fbd2ebacd5c8ac39b1584e968f4a4ca0cd03
Matt Sutkowski <msutkowski@gmail.com>
2021-09-23 14:59:46 -0700

    Use a lookup map for types

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -1,31 +1,33 @@
+import { Setting } from '..';
 import { WorkerMode, WorkerStatus } from '../types';
 
 export const modes: WorkerMode[] = ['normal', 'error'];
 
+type SettingValueMap = {
+  mode: WorkerMode;
+  status: WorkerStatus;
+  delay: string;
+};
+
+type AllowedValueForSetting<Setting> = SettingValueMap[Setting &
+  keyof SettingValueMap];
+
 const buildKey = (prefix: string, key: string) =>
   `${prefix && `${prefix}_`}msw_toolbar_${key}`;
 
-type AllowedValueForSetting<Setting> = Setting extends 'mode'
-  ? WorkerMode
-  : Setting extends 'status'
-  ? WorkerStatus
-  : Setting extends 'delay'
-  ? string
-  : never;
-
-export const get = <Setting extends string>(
+export const get = <S extends Setting>(
   prefix: string,
-  key: Setting,
-  defaultValue?: AllowedValueForSetting<Setting>
+  key: S,
+  defaultValue: AllowedValueForSetting<S>
 ) => {
   const value = localStorage.getItem(buildKey(prefix, key)) ?? defaultValue;
   return value as NonNullable<typeof defaultValue>;
 };
 
-export const set = <Setting extends string>(
+export const set = <S extends Setting>(
   prefix: string,
-  key: Setting,
-  value: AllowedValueForSetting<Setting>
+  key: S,
+  value: AllowedValueForSetting<S>
 ) => {
   localStorage.setItem(buildKey(prefix, key), value);
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,15 +5,24 @@ import { get } from '../helpers';
  * Creates a collection of utils that are scoped to a given application prefix
  */
 export const createUtils = (prefix: string = '') => {
+  /**
+   * A small helper that returns ctx.delay with the value from the toolbar
+   */
   function getDelay(ctx: RestContext) {
     return ctx.delay(Number(get(prefix, 'delay', '0')));
   }
 
   return {
     getDelay,
+    /**
+     * A small helper that returns the given data with the delay from the toolbar
+     */
     json(res: ResponseComposition<any>, ctx: RestContext, data?: any) {
       return res(ctx.json(data), getDelay(ctx));
     },
+    /**
+     * Returns a 404 with the delay from the toolbar
+     */
     notFound(res: ResponseComposition<any>, ctx: RestContext) {
       return res(ctx.status(404), getDelay(ctx));
     },
